eef56c817e4b7ef2d7d0edecece989ed01526f78
binhdevci <ngvanbinhme@gmail.com>
2015-06-13 22:43:55 -0700

    config

--- site/config/config.php
+++ site/config/config.php
@@ -11,7 +11,7 @@
 |	http://example.com/
 |
 */
-$config['base_url']	= "http://".$_SERVER['HTTP_HOST']."/thientam/";
+$config['base_url']	= "http://".$_SERVER['HTTP_HOST']."/";
 
 /*
 |--------------------------------------------------------------------------
